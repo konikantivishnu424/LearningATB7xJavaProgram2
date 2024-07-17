e5c3c7cbbe19801bb8b0ad38303c22e863fb01a2
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-07-17 19:38:51 +0530

    New Files are Added

---
+++ src/ex_13072024/Lab0031.java
@@ -0,0 +1,16 @@
+package ex_13072024;
+
+public class Lab0031 {
+    public static void main(String[] args) {
+        double a =10;
+        double b=24;
+        double sum = a+b;
+        double sub =a-b;
+        double mul = a*b;
+        double div =a/b;
+        System.out.printf("Sum is %f",sum);
+        System.out.printf("Sub is %f",sub);
+        System.out.printf("mul is %f",mul);
+        System.out.printf("div is %f",div);
+    }
+}
