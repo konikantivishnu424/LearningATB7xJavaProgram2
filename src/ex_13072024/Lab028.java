72000470f5e967a19b6542962f935b6cda5166b3
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-07-17 17:31:54 +0530

    New Files are Added

---
+++ src/ex_13072024/Lab028.java
@@ -0,0 +1,8 @@
+package ex_13072024;
+
+public class Lab028 {
+    public static void main(String[] args) {
+        String name ="vishnu";
+        System.out.printf("Your name is -->  %s",name);
+    }
+}
---
+++ src/ex_13072024/Lab029.java
@@ -0,0 +1,16 @@
+package ex_13072024;
+
+public class Lab029 {
+    public static void main(String[] args) {
+        int age = 10;  //Decimal Number System
+        int b=0b0001010;//Binary Automation
+        int octal = 0101; //IF YOUR AAUTOMATION YOu maY BE Working With this number
+        int hex=0xface;
+        //Ascii Table
+        //0-9 48-57;
+        //A-Z 65-90;
+        //a-z 97-122;
+        //0-9 48-57;
+
+    }
+}
