263a62d238603975a439196a4827599f0f252a8f
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-07-18 17:00:38 +0530

    New Files are added

---
+++ src/ex_07072024/Vishnu_123.java
@@ -0,0 +1,7 @@
+package ex_07072024;
+
+public class Vishnu_123 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ex_07072024/Vishnu_vardhan.java
@@ -0,0 +1,7 @@
+package ex_07072024;
+
+public class Vishnu_vardhan {
+    public static void main(String[] args) {
+
+    }
+}
