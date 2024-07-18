3a0fa93c75281805b08a972eb17e5cc2430080f1
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-07-18 16:51:29 +0530

    All Files are Added

--- src/ex_07072024/Lab003.java
+++ src/ex_07072024/Lab003.java
@@ -1,4 +1,19 @@
 package ex_07072024;
 
 public class Lab003 {
+    public static void main(String[] args) {
+        //SIngle line comment
+        //this line code is not Executed
+
+
+        /***
+         *
+         *
+         *
+         *
+         *
+         *
+         * multiple comment Documentation Purpose
+         */
+    }
 }
---
+++ src/ex_07072024/Lab005.java
@@ -0,0 +1,16 @@
+package ex_07072024;
+
+public class Lab005 {
+    public static void main(String[] args) {
+
+
+        System.out.println(2 + 2);
+        System.out.println(2 - 2);
+        System.out.println(2 * 2);
+        System.out.println(2 % 2);
+        System.out.println(10 / 0);
+
+//SOmething Divided by zero is infinity
+        //output Process finished with Exit code 1
+    }
+}
