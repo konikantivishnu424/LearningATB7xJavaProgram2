7875c9dacabee17c869caf16bd0cf719ff1b4e5e
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-07-19 14:44:58 +0530

    New Files are added

---
+++ src/ex_13072024/Lab031.java
@@ -0,0 +1,11 @@
+package ex_13072024;
+
+public class Lab031 {
+    public static void main(String[] args) {
+        final int A=99;
+        //A= 100;
+        System.out.println(A);
+        final String str ="TTA";
+        final Double d1=999.99;
+    }
+}
