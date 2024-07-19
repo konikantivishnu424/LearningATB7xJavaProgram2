2f9be8471287e1861b388875c79a8211eba7f748
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-07-19 11:21:06 +0530

    New Files are added

---
+++ src/ex_13072024/Lab029.java
@@ -0,0 +1,20 @@
+package ex_13072024;
+
+public class Lab029 {
+    public static void main(String[] args) {
+                double a =10;
+                double b=20;
+                double sum =a+b;
+                double sub =a-b;
+                double mul =a*b;
+                double div =a/b;
+        System.out.printf("sum is %f",sum);
+        System.out.println();
+        System.out.printf("sub is %f",sub);
+        System.out.println();
+        System.out.printf("mul is %f",mul);
+        System.out.println();
+        System.out.printf("Div is %f",div);
+
+    }
+}
