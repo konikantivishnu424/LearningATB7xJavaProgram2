ae7268b7ef8fa7a670a399cc2fcdd8a4395c0f99
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-07-19 21:33:48 +0530

    New Files are added

---
+++ src/ex_13072024/Lab037.java
@@ -0,0 +1,13 @@
+package ex_13072024;
+
+public class Lab037 {
+    public static void main(String[] args) {
+        //OPerator
+        int age = 65;
+        //== Assignment operator -Assign the value literal to the Ref of Data types
+        int a=10;
+        int b =24;
+        System.out.println(a+b);
+        //Arithmatic operator +,-,*,/,%
+    }
+}
