376ccf921346b6c33a9e7a1234511571ef9ea980
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-07-19 21:43:28 +0530

    New Files are added

--- src/ex_13072024/Lab039.java
+++ src/ex_13072024/Lab039.java
@@ -2,6 +2,15 @@ package ex_13072024;
 
 public class Lab039 {
     public static void main(String[] args) {
-
+        //Unary operator
+          String firstname ="vishnu";
+        String Lastname ="vardhan";
+        System.out.println(firstname+Lastname);
+        byte a=18;
+        byte b=6;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
     }
 }
