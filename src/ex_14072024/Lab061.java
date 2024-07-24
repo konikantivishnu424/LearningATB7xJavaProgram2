eda693e2ecf956892194fe16303489257de8b4f2
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-07-24 13:00:24 +0530

    Pre and post increment Examples

---
+++ src/ex_14072024/Lab061.java
@@ -0,0 +1,16 @@
+package ex_14072024;
+
+public class Lab061 {
+    public static void main(String[] args) {
+        int a =10;
+        int result = a++;
+        System.out.println(a);
+        System.out.println(result);
+
+
+        int b =10;
+        int result2 = ++b;
+        System.out.println(b);
+        System.out.println(result2);
+    }
+}
