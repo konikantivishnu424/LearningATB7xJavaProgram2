f1486c8b8e7bb44a58e2dc81e054555a629ee236
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-07-24 09:17:15 +0530

    byte and char converted to int program

---
+++ src/ex_14072024/Lab054.java
@@ -0,0 +1,18 @@
+package ex_14072024;
+
+public class Lab054 {
+    public static void main(String[] args) {
+        // byte to int conversion
+
+        byte a = 10;
+        byte b =24;
+        int c = a+b;
+        System.out.println(c);
+
+
+        char a1 ='A';   //A =65;
+        char b2 ='B';   // B =66;
+        int c3 = a1+b2;  // 65+66 =131
+        System.out.println(c3);
+    }
+}
