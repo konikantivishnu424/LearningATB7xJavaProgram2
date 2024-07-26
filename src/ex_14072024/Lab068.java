4d91129392959003d1310290fbc86932258a1c77
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-07-26 15:29:41 +0530

    Assignment operator

---
+++ src/ex_14072024/Lab068.java
@@ -0,0 +1,17 @@
+package ex_14072024;
+
+public class Lab068 {
+    public static void main(String[] args) {
+        //Assignment operator
+        String name = "vishnu"; // Assignment Operators
+        String name2 = new String("vishnu"); // New Operator
+
+        // Both of them, actually save the name, name2 in different ways JVM
+        // How they store the values in the JVM?
+
+
+        // SCP nad Heap - JVM
+
+
+    }
+}
