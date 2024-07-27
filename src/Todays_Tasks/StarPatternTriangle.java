65bc5c2791156d6e7e95d32a5bc7ff9cc149ea89
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-07-27 09:56:29 +0530

    Triangle Star Program

---
+++ src/Todays_Tasks/StarPatternTriangle.java
@@ -0,0 +1,25 @@
+package Todays_Tasks;
+
+public class StarPatternTriangle {
+    public static void main(String[] args) {
+
+//*
+//
+//**
+//
+//***
+//
+//****
+//
+//*****
+//Write a logic program
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println("*");
+        }
+    }
+}
