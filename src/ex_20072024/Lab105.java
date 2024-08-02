04499a82d59ffe3df122c037a0b1ffaa1549b314
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-08-02 19:31:43 +0530

    Vowel or Consonent Program

---
+++ src/ex_20072024/Lab105.java
@@ -0,0 +1,12 @@
+package ex_20072024;
+
+public class Lab105 {
+    public static void main(String[] args) {
+        char ch = 'v';
+        if (ch == 'a' || ch == 'e' || ch == 'o' || ch == 'i' || ch == 'u') {
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
+    }
+}
