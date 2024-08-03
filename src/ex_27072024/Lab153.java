b2829b821f3fbd990782334e327f74254e32deff
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-08-03 07:34:03 +0530

    Reverse a word Program

---
+++ src/ex_27072024/Lab153.java
@@ -0,0 +1,9 @@
+package ex_27072024;
+
+public class Lab153 {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("vishnu");
+        System.out.println(stringBuilder.reverse().toString());
+
+    }
+}
