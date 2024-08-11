c9828551ea451abd36a9866eb48c50977534af1f
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-08-11 08:27:42 +0530

    Building Blueprint Program

---
+++ src/ex_28072024/BuildingBp.java
@@ -0,0 +1,16 @@
+package ex_28072024;
+
+public class BuildingBp {
+   String color;
+   String name;
+   String Address;
+   int noofRooms = 10;
+
+
+   void uselift(){
+       System.out.println("I am using Lift");
+   }
+   void Activities(){
+       System.out.println("I am using Activities");
+   }
+}
