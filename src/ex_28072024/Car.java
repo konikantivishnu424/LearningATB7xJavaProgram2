dbcaa167064dc2f73d1feeb304937041afaf8623
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-08-12 08:29:42 +0530

    Dummy Program

---
+++ src/ex_28072024/Car.java
@@ -0,0 +1,16 @@
+package ex_28072024;
+
+public class Car {
+    public static void main(String[] args) {
+
+    }
+    class person2 {
+
+    }
+    class person3 {
+
+    }
+    class person4 {
+
+    }
+}
