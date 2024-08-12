f547d2d082c680757339279f0606f1cda3fd55cd
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-08-12 05:51:26 +0530

    Dog class and reference Creation Program

---
+++ src/ex_28072024/Dog.java
@@ -0,0 +1,14 @@
+package ex_28072024;
+
+public class Dog {
+    String name;
+    String color;
+    int age;
+    String Breed;
+
+
+    void eat(){}
+    void talk(){}
+    void sleep(){}
+    void walk(){}
+}
---
+++ src/ex_28072024/Lab156.java
@@ -0,0 +1,16 @@
+package ex_28072024;
+
+public class Lab156 {
+    public static void main(String[] args) {
+        Dog Dog1ref= new Dog();
+        Dog1ref.name= "Chow-Chow";
+
+        Dog Dog2ref= new Dog();
+        Dog2ref.name= "German shepard";
+
+
+        //function - perform a task , Block of Code
+        //Functions within class are methods
+        //Class = Blueprint
+    }
+}
