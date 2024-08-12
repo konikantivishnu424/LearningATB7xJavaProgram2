5b593dbe9452ef36fdd416e92f13f9b41b8eb656
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-08-12 08:51:26 +0530

    Animal Program

--- src/ex_28072024/Car.java
+++ src/ex_28072024/Car.java
@@ -1,7 +1,12 @@
 package ex_28072024;
 
 public class Car {
+    //public -- anyone can use it
+    //Car -- identifier
     public static void main(String[] args) {
+        //attributes, data members, instance variable, properties
+
+        //Behaviour, methods, functionalities
 
     }
     class person2 {
