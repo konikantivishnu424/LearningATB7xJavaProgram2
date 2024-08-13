1a2de2566e2faded01b0f230b89390392528824a
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-08-13 16:13:50 +0530

    calling two main methods

--- src/Aug/ex_04082024/Lab167.java
+++ src/Aug/ex_04082024/Lab167.java
@@ -1,16 +1,7 @@
 package Aug.ex_04082024;
 
-public class Lab166 {
-    String name;
-
-
-    void talk(){
-
-    }
-
+public class Lab167 {
     public static void main(String[] args) {
         Lab166 lab166 = new Lab166();
-
     }
-    
 }
