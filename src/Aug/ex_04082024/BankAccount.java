6b81f22d3105c9f4e8fbf8b1c2a22958ccdb2bd9
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-08-15 15:47:40 +0530

    default constructor and paramaterized constructor

---
+++ src/Aug/ex_04082024/BankAccount.java
@@ -0,0 +1,35 @@
+package Aug.ex_04082024;
+
+public class BankAccount {
+
+        String bankname;
+        int balance;
+        String bankcode;
+
+
+        //Dc
+        BankAccount(){
+            bankname="SBI";
+            balance = 100000000;
+            bankcode="SBI001";
+        }
+
+        //pc
+          BankAccount(String bname,int bal,String bcode){
+              this. bankname=bname;
+              this. balance = bal;
+              this. bankcode=bcode;
+        }
+
+        void printDetails(){
+            System.out.println("Bank Name ->" + bankname);
+            System.out.println("Bank Code->" + bankcode);
+            System.out.println("Bal->" + balance);
+
+        }
+    }
+
+
+
+
+
