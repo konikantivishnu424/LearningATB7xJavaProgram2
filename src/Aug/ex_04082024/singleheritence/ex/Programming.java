b3313d773194472bed7681cf6e1af3f9f165f3e5
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-08-17 14:47:00 +0530

    Heritance programming example

---
+++ src/Aug/ex_04082024/singleheritence/ex/Programming.java
@@ -0,0 +1,6 @@
+package Aug.ex_04082024.singleheritence.ex;
+
+public class Programming {
+    int version;
+    String author;
+}
--- src/Aug/ex_04082024/singleheritence/son.java
+++ src/Aug/ex_04082024/singleheritence/son.java
@@ -1,6 +1,7 @@
 package Aug.ex_04082024.singleheritence;
 
 public class son extends Father{
+    // IS A relationship
     void bhk3(){
         System.out.println("3bhk");
     }
