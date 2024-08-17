c6754179eb50758cf025c62f504852a2a8ae9fad
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-08-17 20:30:50 +0530

    Hirarcial Inheritence

---
+++ src/Aug/ex_04082024/HirarcialInheritance/Damu.java
@@ -0,0 +1,8 @@
+package Aug.ex_04082024.HirarcialInheritance;
+
+public class Damu extends Father{
+    void r1(){
+        System.out.println("Damu");
+
+    }
+}
---
+++ src/Aug/ex_04082024/HirarcialInheritance/vishnu.java
@@ -0,0 +1,8 @@
+package Aug.ex_04082024.HirarcialInheritance;
+
+public class vishnu extends Father{
+
+    void h2(){
+        System.out.println("h2-vishnu");
+    }
+}
