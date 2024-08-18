07b65103c5f7a6c8096fba2f3a77be033dd911a2
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2024-08-18 14:50:02 +0530

    isA Relationship

--- src/Aug/ex_04082024/hasA/car.java
+++ src/Aug/ex_04082024/hasA/car.java
@@ -1,7 +1,7 @@
 package Aug.ex_04082024.hasA;
 
 public class car {
-    void Startthecar(){
+    protected void Startthecar(){
         //Aggration Has-A Relationship
         new Engine().start();
         new Tyres().rolling();
---
+++ src/Aug/ex_04082024/hasA/isA/Tesla.java
@@ -0,0 +1,10 @@
+package Aug.ex_04082024.hasA.isA;
+
+import Aug.ex_04082024.hasA.car;
+
+public class Tesla extends car {
+   // isA Relationship
+    void start(){
+        Startthecar();
+    }
+}
